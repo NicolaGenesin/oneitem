1be5147d14904b9b7335d31fa83b94e2e8275159
Nicola Genesin <nicolagenesin@yahoo.it>
2021-01-11 17:03:38 +0100

    Add context

---
+++ context/state.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const Context = createContext();
+
+export function AppWrapper({ children }) {
+  const [state, setState] = useState({ page: 'INITIAL STATE' });
+
+  return (
+    <Context.Provider value={{ state, setState }}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+export function useAppContext() {
+  return useContext(Context);
+}
+
+export function updateContext(data) {
+    const { setState } = useContext(Context);
+
+    useEffect(() => {
+        setState(data);
+    }, [setState]);
+}

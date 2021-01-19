76e0690368b5cfd103d8487b13fba1c6cf09f1a7
Nicola Genesin <nicolagenesin@yahoo.it>
2021-01-19 19:01:41 +0100

    Extend theme

---
+++ config/theme.jsx
@@ -0,0 +1,9 @@
+const colors = {
+  activeButton: '#8eccf7',
+  gray: {
+    50: '#f7fafc',
+    900: '#1a202c',
+  },
+};
+
+export default colors;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
 import App from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { AppWrapper } from '../context/state';
+import colors from '../config/theme';
+
+const theme = extendTheme({ colors });
 
 export default class CustomApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <AppWrapper>
           <Head>
             <title>One9</title>

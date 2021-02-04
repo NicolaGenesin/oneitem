68c3cd6c99d36c0858b806f0eedb7b61882e4a7d
Nicola Genesin <nicolagenesin@yahoo.it>
2021-02-04 08:47:40 +0100

    Add default values to product page

--- components/Product.jsx
+++ components/Product.jsx
@@ -19,7 +19,7 @@ export default function Product({ product, preview }) {
       <Center>
         <Box m="24px">
           <Heading as="h2" size="xl">
-            {product.storeName}
+            {product.storeName || 'Vincent Lab'}
             {' '}
             {preview && <Badge colorScheme="green">{i18n.t('product.preview')}</Badge>}
           </Heading>
@@ -28,13 +28,11 @@ export default function Product({ product, preview }) {
       <Divider orientation="horizontal" />
       <Center mt="5%">
         <Box>
-          {product.author && (
           <Center>
             <Text>
-              {i18n.t('product.author', { author: product.author })}
+              {i18n.t('product.author', { author: product.author || 'Giulia Perin' })}
             </Text>
           </Center>
-          )}
           <HStack mt="48px">
             <Box boxShadow="2xl" mr="48px" rounded="md" maxW="300px">
               <Carousel
@@ -67,20 +65,21 @@ export default function Product({ product, preview }) {
                       boxSize="300px"
                       objectFit="cover"
                       src="https://via.placeholder.com/500?text=　"
+                      // src="https://image.shutterstock.com/image-photo/sky-blue-handbag-purse-beautiful-600w-1174206280.jpg"
                     />
                   )}
               </Carousel>
             </Box>
             <Box maxW="400px">
               <Heading size="xl" mb="16px">
-                {product.name}
+                {product.name || 'Zaino'}
               </Heading>
-              <Text fontSize="md" whiteSpace="pre-line">{product.description}</Text>
+              <Text fontSize="md" whiteSpace="pre-line">{product.description || 'Zaino mini. Foderato internamente e tasca con zip Base in ecopelle. Bretelle regolabili. Chiusura a sacca con coulisse e patta con asola e bottone. Può starci: l\'essenziale'}</Text>
               <HStack mt="24px">
                 <Center>
                   <Heading size="xl" mr="24px">
-                    {product.currency}
-                    {product.price && product.price}
+                    {product.currency || '€'}
+                    {(product.price && product.price) || '35'}
                   </Heading>
                   <Button disabled colorScheme="primaryButton" color="white" size="lg">{i18n.t('product.buy')}</Button>
                 </Center>
@@ -96,7 +95,7 @@ export default function Product({ product, preview }) {
                 One of the world's premier watchmakers, Movado Group, Inc. (MGI) designs, manufactures and distributes watches from nine of the most recognized and respected names in time: our wholly owned Movado, Concord and EBEL brands along with our Coach, HUGO BOSS, Juicy Couture, Lacoste, Tommy Hilfiger and Scuderia Ferrari licensed watch brands.
               </Text> */}
               <Text>
-                {i18n.t('product.contact', { contact: product.contact })}
+                {i18n.t('product.contact', { contact: product.contact || 'hello@vincent.com' })}
               </Text>
             </VStack>
           </Center>

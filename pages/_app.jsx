0220b0c11723393760120150f8b1b1b3b89a9d9d
Nicola Genesin <nicolagenesin@yahoo.it>
2021-02-05 21:01:18 +0100

    Test meta og

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -17,6 +17,14 @@ export default class CustomApp extends App {
           <Head>
             <title>ezyou</title>
             <link rel="icon" href="/favicon.ico" />
+            <meta property="og:title" content="ezyou online shop" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://ezyou.shop" />
+            <meta property="og:image" content="https://www.telotrovo.it/wp-content/uploads/2016/02/siteimage.jpg" />
+            <meta property="og:image:type" content="image/jpeg" />
+            <meta property="og:image:width" content="200" />
+            <meta property="og:image:height" content="200" />
+            <meta property="og:description" content="Site description" />
           </Head>
           <Component {...pageProps} />
         </AppWrapper>

96c4b7e464c320650c9c7cc72c5b3b65bead539b
Nicola Genesin <nicolagenesin@yahoo.it>
2021-02-17 16:15:21 +0100

    Another flask test

--- api/index.py
+++ api/index.py
@@ -86,7 +86,10 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/')
 def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+    return Response("<h1>Flask</h1><p>ROOT: /%s</p>" % (path), mimetype="text/html")
+
+@app.route('/test')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>TEST: /%s</p>" % (path), mimetype="text/html")
